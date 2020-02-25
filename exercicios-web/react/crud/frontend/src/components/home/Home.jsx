0266f5c6afb689a1e7c822c23e5e8403227d0fa4
sdavidbastos <davidtavares98@gmail.com>
2020-02-25 04:12:37 -0300

    Trabalhando com CRUD em react

---
+++ exercicios-web/react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de react." >
+        <div className="display-4">Bem-vindo</div>
+        <hr/>
+        <p className="mb-0">Sistema para facilitar o entendimento a construção de um cadastro desenvolvido em react.</p>
+    </Main>
---
+++ exercicios-web/react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Densenvolvido com <i className="fa fa-heart text-danger"></i>
+                <strong> D<span className="text-danger">av</span>id</strong>
+        </span>
+    </footer>
