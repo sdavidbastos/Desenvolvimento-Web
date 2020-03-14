1a2f21f0032211c5ff2d29146845faaa9409e0af
sdavidbastos <davidtavares98@gmail.com>
2020-03-13 23:09:13 -0300

    Consultas ao banco de dados com Join

--- exercicios-web/banco-relacional/inserirPrefeitos.sql
+++ exercicios-web/banco-relacional/inserirPrefeitos.sql
@@ -4,8 +4,36 @@ values
     ('Rodrigo Neves', 2),
     ('Raquel Lyra', 3),
     ('Zenaldo Coutinho', null)
-select * from prefeitos
-insert into prefeitos 
-    (nome, cidade_id)
+select
+    *
+from
+    prefeitos
+insert into
+    prefeitos (nome, cidade_id)
 values
     ('Rafael Graca', null)
+select
+    *
+from
+    estados
+select
+    *
+from
+    cidades c
+    inner join prefeitos p on c.id = p.cidade_id
+select
+    *
+from
+    cidades c
+    left join prefeitos p on c.id = p.cidade_id
+union
+select
+    *
+from
+    cidades c
+    right join prefeitos p on c.id = p.cidade_id
+select
+    *
+from
+    cidades c full
+    join prfeitos p on c.id = p.cidade_id
