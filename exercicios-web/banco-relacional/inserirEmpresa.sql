7b1085b6cd9212d6ccb20181214c6c0b7f32fe40
sdavidbastos <davidtavares98@gmail.com>
2020-03-16 22:02:22 -0300

    Sql. Trabalhando com duas tabelas e integrando-as

---
+++ exercicios-web/banco-relacional/inserirEmpresa.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+    empresas (nome, cnpj) VALUE ('Bradesco', 05681353000118),
+    ('Vale', 15967197000100),
+    ('Cielo', 01315689000106);
+
+desc empresas desc prefeitos
+select
+    *
+from
+    empresas
+select
+    *
+from
+    cidades
+insert into
+    empresas_unidades (empresa_id, cidade_id, sede)
+values
+    (1, 1, 1),
+    (1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 1);
+
+    desc `empresas_unidades`
